e6e07c4c1877cb300404e8647d97df0ee2111fbe
OleBrum <ole.steinveg@gmail.com>
2016-10-28 12:18:26 +0200

    Add contact tutorial app

---
+++ aurelia_contact_tutorial/src/web-api.js
@@ -0,0 +1,91 @@
+let latency = 200;
+
+let contacts = [
+  {
+    id:1,
+    firstName:'John',
+    lastName:'Tolkien',
+    email:'tolkien@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:2,
+    firstName:'Clive',
+    lastName:'Lewis',
+    email:'lewis@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:3,
+    firstName:'Owen',
+    lastName:'Barfield',
+    email:'barfield@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:4,
+    firstName:'Charles',
+    lastName:'Williams',
+    email:'williams@inklings.com',
+    phoneNumber:'867-5309'
+  },
+  {
+    id:5,
+    firstName:'Roger',
+    lastName:'Green',
+    email:'green@inklings.com',
+    phoneNumber:'867-5309'
+  }
+];
+
+export class WebAPI {
+  isRequesting = false;
+  
+  getContactList(){
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let results = contacts.map(x =>  { return {
+          id:x.id,
+          firstName:x.firstName,
+          lastName:x.lastName,
+          email:x.email
+        }});
+        resolve(results);
+        this.isRequesting = false;
+      }, latency);
+    });
+  }
+
+  getContactDetails(id){
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let found = contacts.filter(x => x.id == id)[0];
+        resolve(JSON.parse(JSON.stringify(found)));
+        this.isRequesting = false;
+      }, latency);
+    });
+  }
+
+  saveContact(contact){
+    this.isRequesting = true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let instance = JSON.parse(JSON.stringify(contact));
+        let found = contacts.filter(x => x.id == contact.id)[0];
+
+        if(found){
+          let index = contacts.indexOf(found);
+          contacts[index] = instance;
+        }else{
+          instance.id = getId();
+          contacts.push(instance);
+        }
+
+        this.isRequesting = false;
+        resolve(instance);
+      }, latency);
+    });
+  }
+}
